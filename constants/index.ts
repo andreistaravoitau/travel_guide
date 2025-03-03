8db310d32fcf8277f7b2672af424c1126d8175c0
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2025-03-03 11:53:38 +0100

    Created Navbar and Button components, declared footer component, established basic layout

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+    {href: "/", key: "home", label: "Home"},
+    {href: "/", key: "about", label: "About"}, 
+    {href: "/", key: "services", label: "Services"},
+    {href: "/", key: "prices", label: "Prices"},
+    {href: "/", key: "contact", label: "Contact"}
+];
